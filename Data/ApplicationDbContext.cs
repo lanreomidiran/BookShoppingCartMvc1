55db66c5a61952805e9fda86b74f02616fb6dfb6
Lanre Omidiran <lanreomidiran@gmail.com>
2026-04-09 09:15:06 +0100

    first commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookShoppingCartMvc1.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    {
+
+    }
+}
